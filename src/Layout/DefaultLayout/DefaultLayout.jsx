c255f82141990631752d20f7ce60282edcb00537
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-13 12:05:15 +0300

    feat: Add responsive fluid layout to DefaultLayout component

--- src/Layout/DefaultLayout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout/DefaultLayout.jsx
@@ -1,18 +1,21 @@
 import { Container } from "react-bootstrap";
 import styles from "./DefaultLayout.module.css";
 import Navbar from "../../components/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { NavbarContext } from "../../context/NavbarContext";
 import { useContext } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
 function DefaultLayout() {
   const { setNewClass } = useContext(NavbarContext);
+  const location = useLocation();
+  const dimensions = useWindowSize();
   return (
     <div>
       <Container fluid={true} className={styles.container22}>
         <Navbar></Navbar>
         <Container
-        fluid={true}
           className={styles.content}
+          fluid={dimensions.width <= 990  ? true : false}
           onClick={() => setNewClass(false)}
         >
           <Outlet />
