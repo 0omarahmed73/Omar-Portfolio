8ec8482a602cf9943036e7fc4e67be3dadf038ef
Omar Ahmed <85828826+0omarahmed73@users.noreply.github.com>
2024-08-22 19:05:20 +0300

    Update CV's Link at Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -101,7 +101,7 @@ function Home() {
               <CustomIcon to="tel:+201553583288">
                 <FaPhoneAlt size={20} />
               </CustomIcon>
-              <CustomIcon to="https://drive.google.com/file/d/1Wo_1NF_4v9ktH9sk8YbaWaXsQ-pnMYoO/view?usp=sharing">
+              <CustomIcon to="https://drive.google.com/file/d/1D2CG9SEu0TTorEXbYuPdQD0XyEXDrqZ3/view">
                 <p
                   style={{
                     all: "unset",
